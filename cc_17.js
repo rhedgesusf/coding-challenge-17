2a53b30abb28f263ecc000894b53a691bcc37639
Riley Hedges <rhedges1@usf.edu>
2025-03-28 19:34:57 -0400

    Task 4 - Build a Client Report System

--- cc_17.js
+++ cc_17.js
@@ -36,6 +36,23 @@ class SalesRep {
         let cl = this.clients.find((element) => element.name === name);
         return cl.getTotalSpent();
     }
+
+    getTotalSpent() {
+        return this.clients.reduce((total, client) =>
+           total + client.getTotalSpent(), 0
+        );
+     }
+
+     getCustomerMinSpend(minAmount) {
+        return this.clients.filter(client => client.getTotalSpent() >= minAmount);
+     }
+
+     getCustomerTotalSpendMap() {
+         return this.clients.map(client => ({
+             name: client.name,
+             totalSpent: client.getTotalSpent()
+         }));
+     }
 }
 
 //////////////////////////////////////
@@ -53,3 +70,43 @@ class VIPCustomer extends Customer {
         return super.getTotalSpent() * loyaltyBonus[this.vipLevel];
     }
 }
+
+//////////////////////////////////////////
+// Task 4: Build a Client Report System //
+//////////////////////////////////////////
+
+let cust = new Customer("Mike Miller", "mikemiller@email.com");
+
+cust.addPurchase(520);
+cust.addPurchase(10);
+cust.addPurchase(1500);
+
+let cust2 = new VIPCustomer("Ethan Pitta", "ethanpitta@email.com", "Platinum");
+
+cust2.addPurchase(111);
+cust2.addPurchase(20);
+cust2.addPurchase(2000);
+
+let cust3 = new VIPCustomer("Nick Diaz", "nickdiaz@email.com", "Gold");
+
+cust3.addPurchase(250);
+cust3.addPurchase(50);
+cust3.addPurchase(900);
+
+console.log(`Total: $${cust.getTotalSpent()}`);
+
+let salesRep = new SalesRep("Joe Johnson");
+
+salesRep.addClient(cust);
+salesRep.addClient(cust2);
+salesRep.addClient(cust3);
+
+console.log(`Mike's Total: $${salesRep.getClientTotal("Mike Miller")}`)
+console.log(`Ethan's Total: $${salesRep.getClientTotal("Ethan Pitta")}`)
+console.log(`Nick's Total: $${salesRep.getClientTotal("Nick Diaz")}`)
+
+console.log(`Total Customer Spent: $${salesRep.getTotalSpent()}`);
+
+console.log(`$500 Min Spent: ${JSON.stringify(salesRep.getCustomerMinSpend(1500))}`);
+
+console.log("Spend Map:", JSON.stringify(salesRep.getCustomerTotalSpendMap()));
