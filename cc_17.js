d76644f9b8d14c588f8ae2f0b79e338b9ab705a5
Riley Hedges <rhedges1@usf.edu>
2025-03-28 18:55:07 -0400

    Task 2 - Create SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -1,3 +1,7 @@
+///////////////////////////////////
+// Task 1: Create Customer Class //
+///////////////////////////////////
+
 class Customer {
     constructor(name, email) {
         this.name = name;
@@ -11,5 +15,25 @@ class Customer {
 
     getTotalSpent() {
         return this.purchaseHistory.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    }
+}
+
+///////////////////////////////////
+// Task 2: Create SalesRep Class //
+///////////////////////////////////
+
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    }
+
+    addClient(customer) {
+        this.clients.push(customer);
+    }
+
+    getClientTotal(name) {
+        let cl = this.clients.find((element) => element.name === name);
+        return cl.getTotalSpent();
     }
 }
